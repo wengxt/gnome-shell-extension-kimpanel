feaf26ae32b58fbe2eb7b1c91f91a24478387798
Weng Xuetian <wengxt@gmail.com>
2011-12-21 08:31:09 +0800

    update name fcitx.org -> kde.org
    Since I have taken the work for kimpanel in KDE,
    so it can be named as kde.org.

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+extension_name="kimpanel@kde.org"
+
 if [ $UID -eq 0 ];then
-	DEST="/usr/share/gnome-shell/extensions/kimpanel@fcitx.org"
+	DEST="/usr/share/gnome-shell/extensions/$extension_name"
 else
-	DEST=$HOME/.local/share/gnome-shell/extensions/kimpanel@fcitx.org/
+	DEST=$HOME/.local/share/gnome-shell/extensions/$extension_name/
 fi
-mkdir -p $DEST
-cp extension.js $DEST
-cp metadata.json $DEST
-cp stylesheet.css $DEST
+mkdir -p $DEST || exit 1
+cp extension.js metadata.json stylesheet.css "$DEST" || exit 1
+echo "Install successfull!"
+echo "Enable it with gnome-tweak-tool"
